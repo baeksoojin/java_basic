b59e817060c3b8e37ac19ba5c4bb6c35d67dc121
sujin-create <qsoojinq@naver.com>
2023-04-15 00:43:54 +0900

    bjo10815/hashset 검색활용

--- src/baekjoon/집합과맵/BOJ10815.java
+++ src/baekjoon/집합과맵/BOJ10815.java
@@ -12,28 +12,15 @@ public class BOJ10815 {
     static int n, m;
     static int arr[];
 
-    private static boolean binarySearch(int num) {
-        int leftIndex = 0;
-        int rightIndex = n - 1;
-
-        while(leftIndex <= rightIndex){
-            int midIndex = (leftIndex + rightIndex) / 2;
-            int mid = arr[midIndex];
-
-            if(num < mid) rightIndex = midIndex - 1;
-            else if(num > mid) leftIndex = midIndex + 1;
-            else return true;
-        }
-        return false;
-    }
-
     public static void answer() throws IOException {
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0 ; i < n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0 ; i < n; i++){
+            set.add(Integer.parseInt(st.nextToken()));
+        }
 
         // 숫자 카드 오름차순 정렬
         Arrays.sort(arr);
@@ -43,7 +30,7 @@ public class BOJ10815 {
 
         for(int i = 0 ; i < m; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if(binarySearch(num)) bw.write("1 ");
+            if(set.contains(num)) bw.write("1 ");
             else bw.write("0 ");
         }
 
