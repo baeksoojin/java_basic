a6be11bf5d174fef76d9fe8d97b43a7111eac822
sujin-create <qsoojinq@naver.com>
2023-04-16 13:43:28 +0900

    재귀에서 반복문으로 변경을 통한 시간복잡도 고려 후 시간초과 해결

--- src/baekjoon/solvedac/class3/BOJ1003.java
+++ src/baekjoon/solvedac/class3/BOJ1003.java
@@ -4,31 +4,23 @@ import java.util.Scanner;
 
 public class BOJ1003 {
 
-    static int countOne = 0;
-    static int countZero = 0;
-
-    public int fibo(int n){
-        if(n==0){
-            countZero += 1;
-            return 0;
-        }else if(n==1){
-            countOne += 1;
-            return 1;
-        }else{
-            return fibo(n-1)+ fibo(n-2);
-        }
-    }
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=0; i<n; i++){
-            int result = new BOJ1003().fibo(sc.nextInt());
-            System.out.println(countZero + " " + countOne);
-            countZero = 0;
-            countOne = 0;
+
+        int[] dp = new int[42];
+        dp[0] = 1;
+        dp[1] = 0;
+        for(int j=2; j<42; j++){
+            dp[j] = dp[j-1] + dp[j-2];
         }
+
+        for(int i=0; i<n; i++){
+            int num = sc.nextInt();
+            System.out.println(dp[num] + " " + dp[num+1]);
+        }
+
 
     }
 }
