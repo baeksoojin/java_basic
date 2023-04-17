2d993e3d3e1abc8703627e64e37eef0b3f67cd7f
sujin-create <qsoojinq@naver.com>
2023-04-17 13:29:26 +0900

    class1문제풀이

---
+++ src/baekjoon/solvedac/class1/BOJ11720.java
@@ -0,0 +1,22 @@
+package baekjoon.solvedac.class1;
+
+import java.util.Scanner;
+
+public class BOJ11720 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String nums = sc.next(); // nextLine()은 개행문자를 제거하지 않아서 2번 사용해줘야함
+
+        int sum=0;
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(nums.substring(i,i+1));
+            sum += num;
+        }
+
+        System.out.println(sum);
+
+    }
+}
